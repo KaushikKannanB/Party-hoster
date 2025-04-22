a9d1f5fb8fbdce4bfda7e41ee4acbb6b63e87123
Kaushik <107951957+kaushikswift@users.noreply.github.com>
2025-04-23 00:16:54 +0530

    added party-details

--- backend/obj/Debug/net7.0/PartyHosting.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/PartyHosting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyHosting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49305847ee0e434aa9046f793e8c49975514dbb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57cd9faecbd1b53c766c5049a46420ede76c8eab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyHosting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyHosting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
