b3ef3f142eaa132ef0e9b14be742b761c0902a9e
Kaushik <107951957+kaushikswift@users.noreply.github.com>
2025-04-23 17:03:58 +0530

    changes in index1

--- backend/obj/Debug/net7.0/PartyHosting.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/PartyHosting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyHosting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4f14e5d2bdec82acedccfe005f7226cd311d35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de57b29f8fe7929f588dea45df99c50f54057cef")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyHosting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyHosting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
